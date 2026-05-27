c5d014554db4d50efdcac63a77def90c6a518e24
Singlehan321 <136765771+Singlehan321@users.noreply.github.com>
2026-05-27 17:04:47 +0330

    style: improve NFT contract formatting

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 ##
+
 pragma solidity ^0.8.0;
 
 contract NFTCollection {
 
 string public collectionName = "Base NFT";
+
 string public collectionSymbol = "BNFT";
+
 uint256 public totalMinted;
 
 function mint() public {
